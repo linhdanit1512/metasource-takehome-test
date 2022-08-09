94e760270d3944df40bb75c22398d8c5a8637db8
Linh Dan Pham <dan.pham@thecollectivec.com>
2022-08-09 17:25:22 +0700

    Init Video player

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# metasource-takehome-test
-Take-Home Test for Frontend Developers
+## Info
+
+- Name: Phạm Trang Linh Đan
+- Email: linhdanit1512@gmail.com
+- Git repo: https://github.com/linhdanit1512/metasource-takehome-test.git
+
+## How to run
+
+1. Clone repository from link: https://github.com/linhdanit1512/metasource-takehome-test.git
+2. Open file **index.html** in browser
+3. Click on video or [Play] button to play the video
+
+Note:
+
+- If you want to change video:
+  1. Open file **main.js** to edit
+  2. Search `defaultVideo:` (Line 21)
+  3. Replace your link
+
+  or You replace the **video-example-3.mp4** file by your video with same name
+  or You can select file input [**Video**] in Form (File duration < 3 min)
+
+<br>
+
+- If you want to change thumbnail:
+  1. Open file **main.js** to edit
+  2. Search `defaultThumbnail:` (Line 22)
+  3. Replace with
+
+  ```js
+    // Have 1 image:
+    defaultThumbnail: '<your-thumbnail>',
+
+    // Have many thumbnails:
+    defaultThumbnail: ['<your-thumbnail1>', '<your-thumbnail2>', '<your-thumbnail3>'],
+  ```
+  or You replace the **thumb1.png** file by your image with same name
+  or You can select file input [**Thumbnail**] in Form
---
+++ main.js
@@ -0,0 +1,494 @@
+const svg = {
+  pause: `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pause-fill" viewBox="0 0 16 16"><path d="M5.5 3.5A1.5 1.5 0 0 1 7 5v6a1.5 1.5 0 0 1-3 0V5a1.5 1.5 0 0 1 1.5-1.5zm5 0A1.5 1.5 0 0 1 12 5v6a1.5 1.5 0 0 1-3 0V5a1.5 1.5 0 0 1 1.5-1.5z"/></svg>`,
+  play: `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"fill="currentColor" class="bi bi-play-fill" viewBox="0 0 16 16"><path d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393z" /></svg>`,
+  sound: `<svg xmlns="http://www.w3.org/2000/svg" width="18" height="16" fill="currentColor" class="bi bi-volume-down-fill" viewBox="0 0 16 16"><path d="M9 4a.5.5 0 0 0-.812-.39L5.825 5.5H3.5A.5.5 0 0 0 3 6v4a.5.5 0 0 0 .5.5h2.325l2.363 1.89A.5.5 0 0 0 9 12V4zm3.025 4a4.486 4.486 0 0 1-1.318 3.182L10 10.475A3.489 3.489 0 0 0 11.025 8 3.49 3.49 0 0 0 10 5.525l.707-.707A4.486 4.486 0 0 1 12.025 8z"/></svg>`,
+  mute: `<svg xmlns="http://www.w3.org/2000/svg" width="18" height="16" fill="currentColor" class="bi bi-volume-mute-fill" viewBox="0 0 16 16"><path d="M6.717 3.55A.5.5 0 0 1 7 4v8a.5.5 0 0 1-.812.39L3.825 10.5H1.5A.5.5 0 0 1 1 10V6a.5.5 0 0 1 .5-.5h2.325l2.363-1.89a.5.5 0 0 1 .529-.06zm7.137 2.096a.5.5 0 0 1 0 .708L12.207 8l1.647 1.646a.5.5 0 0 1-.708.708L11.5 8.707l-1.646 1.647a.5.5 0 0 1-.708-.708L10.793 8 9.146 6.354a.5.5 0 1 1 .708-.708L11.5 7.293l1.646-1.647a.5.5 0 0 1 .708 0z"/></svg>`,
+  fullscreen: `<svg viewBox="0 0 24 24"><path fill="currentColor" d="M5,5H10V7H7V10H5V5M14,5H19V10H17V7H14V5M17,14H19V19H14V17H17V14M10,17V19H5V14H7V17H10Z" /></svg>`,
+  closeFullscreen: `<svg style = "width: 30px; height: 30px" viewBox = "0 0 24 24"><path fill = "currentColor" d = "M14,14H19V16H16V19H14V14M5,14H10V19H8V16H5V14M8,5H10V10H5V8H8V5M19,8V10H14V5H16V8H19Z" /></svg>`
+}
+
+const ScreenBreakPoint = {
+  xs: 416,
+  sm: 576,
+  md: 768,
+  lg: 992,
+  xl: 1200,
+}
+
+function VideoPlayer() {
+  let mediaId = 'your-video'
+  let options = {
+    defaultVideo: 'video-example-3.mp4',
+    defaultThumbnail: 'thumb1.png',
+    defaultTitle: 'Take-Home Test for Frontend Developers',
+    isFullScreen: false,
+    isAddThumbnail: false,
+    mediaId: mediaId,
+    query: {
+      videoElem: `#${mediaId} video`,
+      containerElem: `#${mediaId} .group-media-container`,
+      controlElem: `#${mediaId} .group-media__control`,
+      thumbnailContainer: `#${mediaId} .group-media__thumbnails`,
+    },
+    hidePlayerDuration: 10000,
+    redDotTime: 4, //second
+    thumbnailDisplayDuration: 60, // second
+    buttonPlayCenterDisplayDuration: 500, // 500ms
+  }
+
+  const mediaContainer = document.getElementById(options.mediaId),
+    videoElem = document.querySelector(options.query.videoElem),
+    containerElem = document.querySelector(options.query.containerElem),
+    controlElem = document.querySelector(options.query.controlElem),
+    thumbnailContainer = document.querySelector(options.query.thumbnailContainer)
+  let _self = this, _intervalCount = 0, _volume = 1, _savedElem = {}, _selectedImage, _selectedVideo;
+
+  this.mediaContainer = mediaContainer
+
+  this.init = async function () {
+    await this.bindElem()
+    this.bindEvent()
+  }
+
+  this.bindElem = function () {
+    videoElem.setAttribute('src', options.defaultVideo)
+    document.querySelector('#your-video .video-title').innerHTML = options.defaultTitle
+    document.title = options.defaultTitle
+  }
+
+  this.bindEvent = function () {
+    if (!(videoElem instanceof HTMLVideoElement)) return
+    _bindVideoEvent()
+    _bindControlEvent()
+    _bindInputEvent()
+  }
+
+  this.resetVideo = function () {
+    _setPause(true)
+    _removeAllThumbnail();
+    _setTimer()
+    _addRedDot()
+    _setSound()
+    _intervalCount = 0
+    options.isAddThumbnail = false
+  }
+
+  this.handleResize = function () {
+    _setTimer();
+    _addRedDot();
+  }
+
+  function _bindVideoEvent() {
+    // Update seekbar timer & current time
+    videoElem.addEventListener('timeupdate', function (e) {
+      _setTimer()
+      if (Math.floor(videoElem.currentTime) == options.redDotTime && !options.isAddThumbnail) {
+        options.isAddThumbnail = true
+        _addThumbnail({ elemId: 'thumbnail-test', thumbnail: _selectedImage || options.defaultThumbnail })
+      }
+    })
+
+    // Update duration time
+    videoElem.addEventListener('durationchange', function (e) {
+      _self.resetVideo()
+    })
+
+    // Play/Pause video
+    videoElem.addEventListener('click', function () {
+      _setPause(!videoElem.paused, true)
+    })
+
+    // Show controls, title, shadow
+    let controlDuration = null
+    mediaContainer.addEventListener('mousemove', function () {
+      _showControl(true)
+      // if (controlDuration) {
+      //   clearTimeout(controlDuration)
+      // }
+      // controlDuration = setTimeout(function () {
+      //   _showControl(false)
+      // }, options.hidePlayerDuration)
+    })
+    // Hide controls, title, shadow
+
+  }
+
+  function _bindControlEvent() {
+    // Play/Pause video by button control
+    document.querySelector('#your-video .svg-icon--pause').addEventListener('click', function () {
+      _setPause(!videoElem.paused, true)
+    })
+
+    // Show fullscreen
+    document.querySelector('#your-video .svg-icon--fullscreen').addEventListener('click', function () {
+      _setFullscreen(!options.isFullScreen)
+    })
+
+    document.querySelector('#your-video .svg-icon--volume .icon').addEventListener('click', function () {
+      videoElem.muted = !videoElem.muted
+      _setSound(videoElem.muted ? 0 : _volume)
+    })
+
+    document.querySelector('#your-video input[name="volume"]').addEventListener('input', function () {
+      console.log(this.value);
+      _setSound(this.value)
+    })
+
+    let _timerChangeObj = [
+      '#your-video .group-media__control-seekbar',
+      '#your-video .group-media__control-timer',
+      '#your-video .group-media__control-timer-loaded'
+    ]
+    _timerChangeObj.forEach((elem) => {
+
+      // Change video timer
+      document.querySelector(elem).addEventListener('click', function (e) {
+        let width = e.layerX,
+          seekbarWidth = _getSeekbarWidth(),
+          duration = videoElem.duration;
+
+        let currentTime = width / seekbarWidth * duration;
+        videoElem.currentTime = currentTime
+      })
+
+      // Show tooltip timer
+      document.querySelector(elem).addEventListener('mousemove', function (e) {
+
+        let width = e.layerX,
+          seekbarWidth = _getSeekbarWidth(),
+          duration = videoElem.duration;
+
+        tooltipTime = width / seekbarWidth * duration;
+
+        let tooltip = document.querySelector('#your-video .group-media__control .video-timer-tooltip');
+        if (!tooltip) {
+          tooltip = document.createElement('span')
+          controlElem.append(tooltip)
+        }
+        tooltip.innerText = _formatTime(tooltipTime, true)
+        tooltip.classList.add('video-timer-tooltip')
+        tooltip.style.display = 'block';
+        tooltip.style.left = (width - 15) + 'px'
+      })
+
+      document.querySelector(elem).addEventListener('mouseleave', function () {
+        document.querySelector('#your-video .group-media__control .video-timer-tooltip').style.display = 'none'
+      })
+    })
+
+    // mediaContainer.removeEventListener('mouseleave', function () {
+    //   _showControl(false)
+    // }, true)
+  }
+
+  function _bindInputEvent() {
+    // Form
+    // Change Video title
+    document.querySelector('input[name="video-title"]').addEventListener('input', function (e) {
+      let val = e.target.value
+      document.querySelector('#your-video .video-title').innerHTML = val
+      document.title = val || options.defaultTitle
+    })
+
+
+    // Change thumbnail for video
+    document.querySelector('input[name="thumbnail"]').addEventListener('change', function (e) {
+      if (e.target.files && e.target.files[0]) {
+
+        _selectedImage = []; // Clear old data in selected image
+        let length = e.target.files.length > 4 ? 4 : e.target.files.length
+        for (let i = 0; i < length; i++) {
+          _selectedImage.push(URL.createObjectURL(e.target.files[i]))
+        }
+        if (Object.keys(_savedElem).length > 0) { // Thumbnail's showing
+          _removeAllThumbnail()
+          _addThumbnail({ thumbnail: _selectedImage })
+        }
+
+
+      }
+    })
+
+    document.querySelector('input[name="video"]').addEventListener('change', function (e) {
+      if (e.target.files && e.target.files[0]) {
+        _selectedVideo = URL.createObjectURL(e.target.files[0])
+
+        let videoCheck = document.createElement('video');
+        videoCheck.preload = 'metadata';
+
+        videoCheck.onloadedmetadata = function () {
+
+          window.URL.revokeObjectURL(videoCheck.src);
+
+          if (videoCheck.duration < 4) {
+            alert("Invalid Video! video is less than 4 seconds");
+            return;
+          } else if (videoCheck.duration > 180) {
+            alert("Invalid Video! video is less more than 3 minutes");
+          }
+          else {
+
+            videoElem.setAttribute('src', _selectedVideo)
+          }
+        }
+
+        videoCheck.src = URL.createObjectURL(e.target.files[0]);
+      }
+    })
+
+    document.querySelector('input[name="show_at"]').addEventListener('change', function (e) {
+      options.redDotTime = this.value
+      _addRedDot(this.value)
+
+      if (this.value > videoElem.currentTime) {
+        options.isAddThumbnail = false;
+        _removeAllThumbnail()
+      }
+    })
+
+    document.querySelector('input[name="duration_show"]').addEventListener('change', function (e) {
+      if (videoElem.currentTime > options.redDotTime) {
+        let val = parseFloat(this.value)
+        // new value will extend show
+        if (_intervalCount<  val) {
+          // thumbnail is showing (Object.keys(_savedElem).length > 0)=> ignore 
+          // thumbnail was hidden => add thumbnail
+          if (Object.keys(_savedElem).length == 0) {
+            console.log('need add');
+            _addThumbnail({elemId: 'thumbnail-extended', thumbnail: _selectedImage || options.defaultThumbnail })
+          }
+        } else if (_intervalCount >= val) {
+          console.log('need remove');
+          _removeAllThumbnail()
+        } else {
+          console.log('dahdahjd');
+        }
+      }
+    })
+  }
+
+  function _addRedDot() {
+    // Create indicator red dot
+    document.querySelector('#your-video .group-media__control .group-media__control-indicator')?.remove()
+
+    let indicator = document.createElement('span');
+    let offsetLeft = options.redDotTime / videoElem.duration * _getSeekbarWidth() - 4; // red-dot css has width = 8px
+    indicator.style.left = offsetLeft + 'px'
+    indicator.classList.add('group-media__control-indicator');
+    controlElem.prepend(indicator)
+  }
+
+  function _addThumbnail({ elemId, thumbnail }) {
+    if (!elemId) elemId = 'img__' + (Math.random().toString().replace('.', ''));
+    if (!thumbnail) thumbnail = options.defaultThumbnail;
+    console.log(elemId);
+    _savedElem[elemId] = {
+      thumbnail,
+      timeout: setInterval(function () {
+        if (_intervalCount >= options.thumbnailDisplayDuration || _intervalCount >= videoElem.duration - 2) {
+          _removeThumbnail(elemId)
+        } else {
+          if (!videoElem.paused) {
+            _intervalCount++;
+          }
+        }
+      }, 1000)
+    }
+
+    //Insert thumbnail to video
+    let thumbnailsContainer = document.querySelector('#your-video .group-media__thumbnails')
+    if (!thumbnailsContainer) {
+      let temp = document.createElement('div')
+      temp.classList.add('group-media__thumbnails')
+      containerElem.prepend(temp)
+    }
+
+    function addImage(id, image) {
+      let thumbnailElem = document.createElement('div')
+      thumbnailElem.id = id
+      thumbnailElem.classList.add('thumbnail-item')
+      thumbnailElem.innerHTML = `<img src="${image}">`
+
+      document.querySelector('#your-video .group-media__thumbnails').append(thumbnailElem)
+    }
+
+    if (thumbnail instanceof Array) {
+      thumbnail.forEach((item, i) => {
+        addImage(elemId + '__' + i, item)
+      })
+
+    } else if (typeof thumbnail == 'string') {
+      addImage(elemId, thumbnail)
+    }
+  }
+
+  function _showControl(val) {
+    let elems = [
+      '#your-video .video-title',
+      '#your-video .group-media__control',
+      '#your-video .group-media-shadow--bottom',
+      '#your-video .group-media-shadow--top'
+    ]
+    if (val) {
+      elems.forEach(elem => {
+        document.querySelector(elem).classList.add('show')
+      })
+
+    } else {
+      elems.forEach(elem => {
+        document.querySelector(elem).classList.remove('show')
+      })
+    }
+
+  }
+
+  function _setTimer() {
+    let current = videoElem.currentTime,
+      duration = videoElem.duration,
+      width = _getSeekbarWidth(),
+      currentWidth = current / duration * width + 'px';
+
+    document.querySelector('#your-video .group-media__control-timer').style.width = currentWidth
+    document.querySelector('#your-video .video-timer__video-length').innerHTML = _formatTime(duration)
+    document.querySelector('#your-video .video-timer__current').innerHTML = _formatTime(current)
+    if (current != 0) {
+      document.querySelector('#your-video .group-media__control-timer').classList.add('run')
+    }
+  }
+
+  function _setSound(val) {
+    if (val == undefined) {
+      val = videoElem.volume * 10
+    }
+    if (val > 10) val = 10
+
+    _volume = val;
+    if (val < 1) {
+      videoElem.volume = 0;
+      videoElem.muted = true
+      document.querySelector('#your-video .svg-icon--volume .icon').innerHTML = svg.mute
+
+    } else {
+      videoElem.volume = Math.round(val) / 10
+      videoElem.muted = false
+      document.querySelector('#your-video .svg-icon--volume .icon').innerHTML = svg.sound
+    }
+  }
+
+  function _setPause(val, videoControl) {
+    videoElem.paused = !videoElem.paused
+
+    if (val) {
+      if (videoControl) {
+        videoElem.pause()
+
+        let button = document.createElement('button')
+        button.className += 'video-center-pause pause'
+        button.innerHTML = svg.play
+        containerElem.append(button)
+
+        setTimeout(function () {
+          button.remove()
+
+        }, options.buttonPlayCenterDisplayDuration)
+      }
+      document.querySelector('#your-video .svg-icon--pause').innerHTML = svg.play
+
+    } else {
+      if (videoControl) {
+        videoElem.play()
+        let button = document.createElement('button')
+        button.className += 'video-center-pause pause'
+        button.innerHTML = svg.pause
+        containerElem.append(button)
+
+        setTimeout(function () {
+          button.remove()
+        }, options.buttonPlayCenterDisplayDuration)
+      }
+      document.querySelector('#your-video .svg-icon--pause').innerHTML = svg.pause
+    }
+  }
+
+  function _setFullscreen(val = false) {
+    options.isFullScreen = val;
+    let videoFrame = mediaContainer
+    if (val) {
+      if (videoFrame.requestFullscreen) {
+        videoFrame.requestFullscreen();
+      } else if (videoFrame.webkitRequestFullscreen) { /* Safari */
+        videoFrame.webkitRequestFullscreen();
+      } else if (videoFrame.msRequestFullscreen) { /* IE11 */
+        videoFrame.msRequestFullscreen();
+      }
+
+      document.querySelector('#your-video .svg-icon--fullscreen').innerHTML = svg.closeFullscreen
+
+    } else {
+      if (document.exitFullscreen) {
+        document.exitFullscreen();
+      } else if (document.webkitExitFullscreen) { /* Safari */
+        document.webkitExitFullscreen();
+      } else if (document.msExitFullscreen) { /* IE11 */
+        document.msExitFullscreen();
+      }
+
+      document.querySelector('#your-video .svg-icon--fullscreen').innerHTML = svg.fullscreen
+
+    }
+  }
+
+  function _removeThumbnail(elemId) {
+    if (!elemId) return
+    document.getElementById(elemId)?.remove()
+    if (_savedElem[elemId]?.timeout) {
+      clearInterval(_savedElem[elemId].timeout)
+      delete _savedElem[elemId]
+    }
+  }
+
+  function _removeAllThumbnail() {
+    for (elemId in _savedElem) {
+      _removeThumbnail(elemId)
+      thumbnailContainer.innerHTML = ''
+    }
+  }
+
+  function _getSeekbarWidth() {
+    let videoPadding = parseFloat(document.querySelector('#your-video')
+      .computedStyleMap()
+      .getAll('padding')
+      .toString()
+      .replace(/px/, ''))
+    return videoElem.offsetWidth - videoPadding
+  }
+
+  function _formatTime(time) {
+    let result = []
+    let min = 0, sec = 0;
+
+    if (time / 60 >= 1) {
+      min = Math.floor(time / 60)
+      time = time % 60
+    }
+
+    sec = Math.round(time);
+
+    if (min > 9) result.push(min); else result.push('0' + min)
+    if (sec > 9) result.push(sec); else result.push('0' + sec)
+
+    return result.join(':')
+  }
+
+  return this
+}
+
+
+let videoPlayer = new VideoPlayer()
+window.addEventListener('DOMContentLoaded', function () {
+  videoPlayer.init()
+})
+
+window.addEventListener('resize', function () {
+  videoPlayer.handleResize()
+})
